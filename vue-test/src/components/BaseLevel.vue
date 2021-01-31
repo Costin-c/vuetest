c28d7b41617dd5565d3d74b3705afeb919c08cb7
costin <2398925401@qq.com>
2021-01-31 09:51:24 +0800

    53

--- vue-test/src/components/BaseLevel.vue
+++ vue-test/src/components/BaseLevel.vue
@@ -1,45 +1,38 @@
-
+<template functional>
+  <div>
+    {{ props.level }}
+    <slot :text="'hello'"></slot>
+  </div>
+</template>
 
 <script>
-// <template>
-//   <h1 v-if="level === 1">
-//     <slot></slot>
-//   </h1>
-//   <h2 v-else-if="level === 2">
-//     <slot></slot>
-//   </h2>
-//   <h3 v-else-if="level === 3">
-//     <slot></slot>
-//   </h3>
-//   <h4 v-else-if="level === 4">
-//     <slot></slot>
-//   </h4>
-//   <h5 v-else-if="level === 5">
-//     <slot></slot>
-//   </h5>
-//   <h6 v-else>
-//     <slot></slot>
-//   </h6>
-// </template>
-
-
 export default {
+  functional: true,
+  inject: ['name'],
   props: {
     level: {
       type: Number,
       required: true,
-    },
+    }
   },
-  render (createElement) {
-    const vNode = createElement('h'+this.level, this.$slots.default);
-    console.log(vNode);
-    return vNode;
-  },
-}
+  render (h, context) {
 
-var person = {
-  name: 'shanshan',
-  age: 18,
-  looks: 'beatifull',
+    const { props, slots, scopedSlots, data, parent, listeners, injections, children } = context;
+    const tag = 'h' + props.level;
+
+    console.log(children);
+
+    return (
+      <tag>
+        { 
+          // slots().default 
+        }
+        { scopedSlots.header({ text: '具名插槽'}) }
+        { scopedSlots.default({ text: '默认插槽'}) }
+        { injections.name }
+
+      </tag>
+    )
+  },
 }
 </script>
